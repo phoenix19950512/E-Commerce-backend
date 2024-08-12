13e6b6fbba58ee56dc29128724785db5f09b5153
dragonwarrior61 <amodfired@gmail.com>
2024-08-12 12:45:50 +0300

    test review

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_utils.tasks import repeat_every
 from sqlalchemy import select
-from app.routers import auth, internal_products, returns, users, shipment, profile, marketplace, utils, orders, dashboard, supplier, inventory, AWB_generation, notifications, customer, warehouse, team_member, locality, courier
+from app.routers import auth, internal_products, returns, users, shipment, profile, marketplace, utils, orders, dashboard, supplier, inventory, AWB_generation, notifications, customer, warehouse, team_member, locality, courier, review
 from app.database import Base, engine
 from app.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -72,6 +72,7 @@ app.include_router(warehouse.router, prefix="/api/warehouse", tags=["warehouses"
 app.include_router(team_member.router, prefix="/api/team_member", tags=["team_member"])
 app.include_router(locality.router, prefix="/api/locality", tags=['locality'])
 app.include_router(courier.router, prefix="/api/courier", tags=['courier'])
+app.include_router(review.router, prefix="/api/reiew", tags=["review"])
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
