344031e5313d2729d49e7d8ff482a56649cc9bc2
dragonwarrior61 <amodfired@gmail.com>
2024-08-23 13:12:41 +0300

    update awb

--- app/routers/AWB_generation.py
+++ app/routers/AWB_generation.py
@@ -99,9 +99,10 @@ async def create_awbs_(awb: AWBCreate, marketplace: str, db: AsyncSession = Depe
     db_awb.reservation_id = results.get('reservation_id') if results.get('reservation_id') else 0
     db_awb.courier_id = results.get('courier_id') if results.get('courier_id') else 0
     db_awb.courier_name = results.get('courier_name') if results.get('courier_name') else ""
-    result_awb = results.get('awb')
-    db_awb.awb_number = result_awb[0].get('awb_number') if result_awb else ""
-    db_awb.awb_barcode = result_awb[0].get('awb_barcode') if result_awb else ""
+    if results.get('awb'):
+        result_awb = results.get('awb')[0]
+        db_awb.awb_number = result_awb.get('awb_number') if result_awb.get('awb_number') else ""
+        db_awb.awb_barcode = result_awb.get('awb_barcode') if result_awb.get('awb_barcode') else ""
 
     db.add(db_awb)
     await db.commit()
