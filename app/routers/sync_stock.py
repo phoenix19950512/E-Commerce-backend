224e5481ae4a1a22432573be1a69de2973e20822
dragonwarrior61 <amodfired@gmail.com>
2024-08-26 17:56:07 +0300

    update stock sync

---
+++ app/routers/sync_stock.py
@@ -0,0 +1,93 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import func, any_
+from typing import List
+from app.database import get_db
+from app.models.internal_product import Internal_Product
+from app.models.orders import Order
+from app.models.product import Product
+from app.models.marketplace import Marketplace
+from app.utils.altex_product import post_stock_altex
+from app.utils.emag_products import post_stock_emag
+from sqlalchemy import update
+import logging
+
+router = APIRouter()
+
+@router.get("/")
+async def send_stock(
+    db: AsyncSession = Depends(get_db)
+):
+    logging.info("Init orders_stock")
+    await db.execute(update(Internal_Product).values(orders_stock=0))
+    await db.commit()
+    logging.info("Calculate orders_stock")
+    result = await db.execute(select(Order).where(Order.status == any_([1,2,3])))
+    new_orders = result.scalars().all()
+    if new_orders is None:
+        logging.info("Can't find new orders")
+        return
+    logging.info(f"Find {len(new_orders)} new orders")
+
+    try:
+        for new_order in new_orders:
+            product_id_list = new_order.product_id
+            quantity_list = new_order.quantity
+            marketplace = new_order.order_market_place
+            logging.info(f"@#@#!#@#@##!@#@#@ order_id is {new_order.id}")
+            for i in range(len(product_id_list)):
+                product_id = product_id_list[i]
+                quantity = quantity_list[i]
+            
+                result = await db.execute(select(Product).where(Product.id == product_id, Product.product_marketplace == marketplace))
+                db_product = result.scalars().first()
+
+                if db_product is None:
+                    logging.info(f"Can't find {product_id} in {marketplace}")
+                    continue
+                ean = db_product.ean
+                logging.info(f"&*&*&*&&*&*&**&ean number is {ean}")
+
+                result = await db.execute(select(Internal_Product).where(Internal_Product.ean == ean))
+                db_internal_product = result.scalars().first()
+
+                if db_internal_product is None:
+                    logging.info(f"Can't find {ean}")
+                    continue
+                db_internal_product.orders_stock = db_internal_product.orders_stock + quantity
+                # logging.info(f"#$$$#$#$#$#$ Orders_stock is {db_internal_product.orders_stock}")
+                await db.commit()
+                await db.refresh(db_internal_product)
+                logging.info(f"#$$$#$#$#$#$ Orders_stock is {db_internal_product.orders_stock}")
+        logging.info("Sync stock")
+        result = await db.execute(select(Internal_Product))
+        products = result.scalars().all()
+        for product in products:
+            if product.smartbill_stock is None:
+                continue
+            ean = product.ean
+            marketplaces = product.market_place
+            for domain in marketplaces:
+                result = await db.execute(select(Marketplace).where(Marketplace.marketplaceDomain == domain))
+                marketplace = result.scalars().first()
+                
+                result = await db.execute(select(Product).where(Product.ean == ean, Product.product_marketplace == domain))
+                db_product = result.scalars().first()
+                product_id = db_product.id
+                stock = product.smartbill_stock - product.orders_stock - product.damaged_goods
+
+                if marketplace.marketplaceDomain == "altex.ro":
+                    if db_product.barcode_title == "":
+                        continue
+                    post_stock_altex(marketplace, db_product.barcode_title, stock)
+                    logging.info("post stock success in altex")
+                else:
+                    post_stock_emag(marketplace, product_id, stock)
+                    logging.info("post stock success in emag")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        await db.rollback() 
+
+
--- refresh_data.py
+++ refresh_data.py
@@ -98,26 +98,26 @@ async def on_startup():
 #                     # await refresh_emag_all_orders(marketplace, session)
 #                     continue
 
-@app.on_event("startup")
-@repeat_every(seconds=900)
-async def refresh_orders_data(db:AsyncSession = Depends(get_db)):
-    async for db in get_db():
-        async with db as session:
-            logging.info("Starting orders refresh")
-            result = await session.execute(select(Marketplace).order_by(Marketplace.id.asc()))
-            marketplaces = result.scalars().all()
-            logging.info(f"Success getting {len(marketplaces)} marketplaces")
-            for marketplace in marketplaces:
-                if marketplace.marketplaceDomain == "altex.ro":
-                    logging.info("Refresh products from marketplace")
-                    await refresh_altex_products(marketplace)
-                    logging.info("Refresh orders from marketplace")
-                    await refresh_altex_orders(marketplace)
-                else:
-                    logging.info("Refresh products from marketplace")
-                    await refresh_emag_products(marketplace)
-                    logging.info("Refresh orders from marketplace")
-                    await refresh_emag_orders(marketplace)
+# @app.on_event("startup")
+# @repeat_every(seconds=900)
+# async def refresh_orders_data(db:AsyncSession = Depends(get_db)):
+#     async for db in get_db():
+#         async with db as session:
+#             logging.info("Starting orders refresh")
+#             result = await session.execute(select(Marketplace).order_by(Marketplace.id.asc()))
+#             marketplaces = result.scalars().all()
+#             logging.info(f"Success getting {len(marketplaces)} marketplaces")
+#             for marketplace in marketplaces:
+#                 if marketplace.marketplaceDomain == "altex.ro":
+#                     logging.info("Refresh products from marketplace")
+#                     await refresh_altex_products(marketplace)
+#                     logging.info("Refresh orders from marketplace")
+#                     await refresh_altex_orders(marketplace)
+#                 else:
+#                     logging.info("Refresh products from marketplace")
+#                     await refresh_emag_products(marketplace)
+#                     logging.info("Refresh orders from marketplace")
+#                     await refresh_emag_orders(marketplace)
 
 # @app.on_event("startup")
 # @repeat_every(seconds=900)
