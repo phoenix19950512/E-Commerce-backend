8178d7764b5190a51f9798ae1ebb17b0219ec241
dragonwarrior61 <amodfired@gmail.com>
2024-09-10 10:17:46 -0400

    update replacement using order_id

--- app/routers/replacement.py
+++ app/routers/replacement.py
@@ -103,6 +103,58 @@ async def get_replacements(
     
     return replacement_data
 
+@router.get("/order_id")
+async def get_replacements_order(
+    page: int = Query(1, ge=1, description="Page number"),
+    itmes_per_page: int = Query(50, ge=1, le=100, description="Number of items per page"),
+    status: int = Query(0, description="status"),
+    order_id: int = Query(...),
+    db: AsyncSession = Depends(get_db)
+):
+    AWBAlias = aliased(AWB)
+    InvoiceAlias = aliased(Invoice)
+    OrderAlias = aliased(Order)
+    query = select(Replacement, AWBAlias, InvoiceAlias, OrderAlias).outerjoin(
+        AWBAlias,
+        and_(
+            Replacement.order_id == AWBAlias.order_id,
+            Replacement.number == -AWBAlias.number
+        )
+    ).outerjoin(
+        InvoiceAlias,
+        InvoiceAlias.replacement_id == Replacement.id
+    ).outerjoin(
+        OrderAlias,
+        OrderAlias.id == Replacement.order_id
+    ).where(Replacement.order_id == order_id)
+    if status == 1:
+        query = query.where(AWBAlias.order_id.is_(None))
+    offset = (page - 1) * itmes_per_page
+    result = await db.execute(query.offset(offset).limit(itmes_per_page))
+    db_replacements = result.all()
+
+    if db_replacements is None:
+        raise HTTPException(status_code=404, detail="replacement not found")
+    
+    replacement_data = []
+    for replacement, awb, invoice, order in db_replacements:
+        ean = []
+        if order is not None:
+            product_ids = order.product_id
+            for product_id in product_ids:
+                result = await db.execute(select(Product).where(Product.id == product_id, Product.product_marketplace == order.order_market_place))
+                product = result.scalars().first()
+                ean.append(product.ean)
+        replacement_data.append({
+            **{column.name: getattr(replacement, column.name) for column in replacement.__table__.columns},
+            "awb": awb,
+            "invoice": invoice,
+            "order": order,
+            "ean": ean
+        })
+    
+    return replacement_data
+
 @router.get("/{replacement_id}", response_model=ReplacementsRead)
 async def get_replacement(replacement_id: int, db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Replacement).filter(Replacement.id == replacement_id))
