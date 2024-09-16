c8cdba6d2e32205bca34e52bf29e9cf2577c5161
dragonwarrior61 <amodfired@gmail.com>
2024-09-16 01:31:58 -0400

    update refresh_data

--- refresh_data.py
+++ refresh_data.py
@@ -22,6 +22,7 @@ from app.utils.altex_returns import refresh_altex_rmas
 from app.utils.altex_location import refresh_altex_locations
 from app.utils.stock_sync import calc_order_stock
 from app.utils.smart_api import get_stock
+from app.utils.sameday import tracking
 from app.routers.reviews import *
 from app.models.awb import AWB
 from app.models.marketplace import Marketplace
@@ -198,10 +199,16 @@ async def refresh_data(db: AsyncSession = Depends(get_db)):
 async def update_awb(db:AsyncSession = Depends(get_db)):
     async for db in get_db():
         async with db as session:
-            awb_status_list = [56, 85, 84, 9, 37, 63, 1, 2, 25, 33, 7, 78, 6, 26, 14, 23, 35, 79, 93, 112, 81, 10, 113, 27, 87, 4, 99, 16, 74, 116, 15, 18, 61, 111, 57, 137, 82, 3, 11, 28, 127, 17, 
+            awb_status_list = [56, 85, 84, 37, 63, 1, 2, 25, 33, 7, 78, 6, 26, 14, 23, 35, 79, 93, 112, 81, 10, 113, 27, 87, 4, 99, 74, 116, 15, 18, 61, 111, 57, 137, 82, 3, 11, 28, 127, 17, 
 68, 101, 147, 73, 126, 47, 145, 128, 19]
             logging.info("Update awb status")
-            result = await session.execute(select(AWB).where(AWB.awb_status == any_(awb_status_list)))
+            result = await session.execute(select(AWB.awb_number).where(AWB.awb_status == any_(awb_status_list)))
+            awb_numbers = result.scalars.all()
+            for awb_number in awb_numbers:
+                await tracking(awb_number)
+            await session.commit()
+            logging.info("Update awb_status successfully")
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("refresh_data:app", host="0.0.0.0", port=3000, reload=False)
