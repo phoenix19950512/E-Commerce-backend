cb2ae7bc5fe5d89e317f2afe367eeaa451b958c1
dragonwarrior61 <amodfired@gmail.com>
2024-09-20 17:34:07 -0400

    not testing

--- refresh_data.py
+++ refresh_data.py
@@ -90,11 +90,10 @@ async def refresh_orders_data(db:AsyncSession = Depends(get_db)):
             for marketplace in marketplaces:
                 if marketplace.marketplaceDomain == "altex.ro":
                     logging.info("Refresh products from marketplace")
-                    # await refresh_altex_products(marketplace)
+                    await refresh_altex_products(marketplace)
                     logging.info("Refresh orders from marketplace")
                     await refresh_altex_orders(marketplace)
                 else:
-                    continue
                     logging.info("Refresh products from marketplace")
                     await refresh_emag_products(marketplace)
                     logging.info("Refresh orders from marketplace")
@@ -138,116 +137,116 @@ async def refresh_orders_data(db:AsyncSession = Depends(get_db)):
 #             logging.error(f"An error occurred: {e}")
 #             await session.rollback()                
 
-# @app.on_event("startup")
-# @repeat_every(seconds=7200)
-# async def refresh_stock(db: AsyncSession = Depends(get_db)):
-#     async for db in get_db():
-#         async with db as session:
-#             logging.info("Starting stock refresh")
-#             result = await session.execute(select(Billing_software))
-#             db_smarts = result.scalars().all()
-#             if db_smarts is None:
-#                 logging.info("Can't find billing software")
-#             else:
-#                 logging.info("Fetch stock via smarbill api")
-#                 product_code_list = []
-#                 for db_smart in db_smarts:
-#                     products_list = get_stock(db_smart)
-#                     for products in products_list:
-#                         products = products.get('products')
-#                         for product in products:
-#                             logging.info(product)
-#                             product_code = product.get('productCode')
-#                             logging.info(f"Update stock {product_code}")
-#                             result = await session.execute(select(Internal_Product).where(Internal_Product.product_code == product_code))
-#                             db_product = result.scalars().first()
-#                             if db_product is None:
-#                                 product_code_list.append({
-#                                     "product_code": product_code,
-#                                     "quantity": int(product.get('quantity'))
-#                                 })
-#                                 continue
-#                             db_product.smartbill_stock = int(product.get('quantity'))
-#                             await session.commit()
-#                             await session.refresh(db_product)
-#                 logging.info(f"product_code_list: {product_code_list}")
-#                 logging.info("Finish sync stock")
+@app.on_event("startup")
+@repeat_every(seconds=7200)
+async def refresh_stock(db: AsyncSession = Depends(get_db)):
+    async for db in get_db():
+        async with db as session:
+            logging.info("Starting stock refresh")
+            result = await session.execute(select(Billing_software))
+            db_smarts = result.scalars().all()
+            if db_smarts is None:
+                logging.info("Can't find billing software")
+            else:
+                logging.info("Fetch stock via smarbill api")
+                product_code_list = []
+                for db_smart in db_smarts:
+                    products_list = get_stock(db_smart)
+                    for products in products_list:
+                        products = products.get('products')
+                        for product in products:
+                            logging.info(product)
+                            product_code = product.get('productCode')
+                            logging.info(f"Update stock {product_code}")
+                            result = await session.execute(select(Internal_Product).where(Internal_Product.product_code == product_code))
+                            db_product = result.scalars().first()
+                            if db_product is None:
+                                product_code_list.append({
+                                    "product_code": product_code,
+                                    "quantity": int(product.get('quantity'))
+                                })
+                                continue
+                            db_product.smartbill_stock = int(product.get('quantity'))
+                            await session.commit()
+                            await session.refresh(db_product)
+                logging.info(f"product_code_list: {product_code_list}")
+                logging.info("Finish sync stock")
 
-# @app.on_event("startup")
-# @repeat_every(seconds=86400)  # Run daily for deleting video last 30 days
-# async def refresh_data(db: AsyncSession = Depends(get_db)): 
-#     async for db in get_db():
-#         async with db as session:
-#             logging.info("Starting product refresh")
-#             result = await session.execute(select(Marketplace).order_by(Marketplace.id.asc()))
-#             marketplaces = result.scalars().all()
-#             logging.info(f"Success getting {len(marketplaces)} marketplaces")
-#             for marketplace in marketplaces:
-#                 if marketplace.marketplaceDomain == "altex.ro":
-#                     logging.info("Refresh rmas from altex")
-#                     await refresh_altex_rmas(marketplace)
-#                     continue
-#                 else:
-#                     logging.info("Refresh refunds from marketplace")
-#                     await refresh_emag_returns(marketplace)
-#                     # logging.info("Refresh reviews from emag")
-#                     # await refresh_emag_reviews(marketplace, session)
-#                     logging.info("Check hijacker and review")
-#                     await check_hijacker_and_bad_reviews(marketplace, session)
+@app.on_event("startup")
+@repeat_every(seconds=86400)  # Run daily for deleting video last 30 days
+async def refresh_data(db: AsyncSession = Depends(get_db)): 
+    async for db in get_db():
+        async with db as session:
+            logging.info("Starting product refresh")
+            result = await session.execute(select(Marketplace).order_by(Marketplace.id.asc()))
+            marketplaces = result.scalars().all()
+            logging.info(f"Success getting {len(marketplaces)} marketplaces")
+            for marketplace in marketplaces:
+                if marketplace.marketplaceDomain == "altex.ro":
+                    logging.info("Refresh rmas from altex")
+                    await refresh_altex_rmas(marketplace)
+                    continue
+                else:
+                    logging.info("Refresh refunds from marketplace")
+                    await refresh_emag_returns(marketplace)
+                    # logging.info("Refresh reviews from emag")
+                    # await refresh_emag_reviews(marketplace, session)
+                    logging.info("Check hijacker and review")
+                    await check_hijacker_and_bad_reviews(marketplace, session)
 
-# @app.on_event("startup")
-# @repeat_every(seconds=14400)
-# async def update_awb(db: AsyncSession = Depends(get_db)):
-#     async for db in get_db():
-#         async with db as session:
-#             awb_status_list = [56, 85, 84, 37, 63, 1, 2, 25, 33, 7, 78, 6, 26, 14, 23, 35, 79, 93, 112, 81, 10, 113, 27, 87, 4, 99, 74, 116, 15, 18, 61, 111, 57, 137, 82, 3, 11, 28, 127, 17,
-#                             68, 101, 147, 73, 126, 47, 145, 128, 19]
-#             logging.info("Start updating AWB status")
+@app.on_event("startup")
+@repeat_every(seconds=14400)
+async def update_awb(db: AsyncSession = Depends(get_db)):
+    async for db in get_db():
+        async with db as session:
+            awb_status_list = [56, 85, 84, 37, 63, 1, 2, 25, 33, 7, 78, 6, 26, 14, 23, 35, 79, 93, 112, 81, 10, 113, 27, 87, 4, 99, 74, 116, 15, 18, 61, 111, 57, 137, 82, 3, 11, 28, 127, 17,
+                            68, 101, 147, 73, 126, 47, 145, 128, 19]
+            logging.info("Start updating AWB status")
 
-#             batch_size = 100
-#             offset = 0
-#             while True:
-#                 try:
-#                     result = await session.execute(
-#                         select(AWB)
-#                         .where(AWB.awb_status == any_(awb_status_list))
-#                         .offset(offset)
-#                         .limit(batch_size)
-#                     )
-#                     db_awbs = result.scalars().all()
+            batch_size = 100
+            offset = 0
+            while True:
+                try:
+                    result = await session.execute(
+                        select(AWB)
+                        .where(AWB.awb_status == any_(awb_status_list))
+                        .offset(offset)
+                        .limit(batch_size)
+                    )
+                    db_awbs = result.scalars().all()
 
-#                     if not db_awbs:
-#                         break
+                    if not db_awbs:
+                        break
 
-#                     for awb in db_awbs:
-#                         awb_number = awb.awb_number
-#                         try:
-#                             # Track and update awb status
-#                             awb_status = await tracking(awb_number)
-#                             if awb_status is not None:
-#                                 awb.awb_status = awb_status
-#                             else:
-#                                 logging.error(f"Invalid status for AWB {awb_number}, skipping update")
-#                         except Exception as track_ex:
-#                             logging.error(f"Tracking API error for AWB {awb_number}: {str(track_ex)}")
-#                             continue  # Continue to next AWB if tracking fails
+                    for awb in db_awbs:
+                        awb_number = awb.awb_number
+                        try:
+                            # Track and update awb status
+                            awb_status = await tracking(awb_number)
+                            if awb_status is not None:
+                                awb.awb_status = awb_status
+                            else:
+                                logging.error(f"Invalid status for AWB {awb_number}, skipping update")
+                        except Exception as track_ex:
+                            logging.error(f"Tracking API error for AWB {awb_number}: {str(track_ex)}")
+                            continue  # Continue to next AWB if tracking fails
 
-#                     try:
-#                         await session.commit()
-#                         logging.info(f"Successfully committed batch starting from offset {offset}")
-#                     except Exception as e:
-#                         await session.rollback()
-#                         logging.error(f"Failed to commit batch at offset {offset}: {str(e)}")
-#                         break
+                    try:
+                        await session.commit()
+                        logging.info(f"Successfully committed batch starting from offset {offset}")
+                    except Exception as e:
+                        await session.rollback()
+                        logging.error(f"Failed to commit batch at offset {offset}: {str(e)}")
+                        break
 
-#                     offset += batch_size
+                    offset += batch_size
 
-#                 except Exception as db_ex:
-#                     logging.error(f"Database query failed at offset {offset}: {str(db_ex)}")
-#                     await session.rollback()
-#                     break
+                except Exception as db_ex:
+                    logging.error(f"Database query failed at offset {offset}: {str(db_ex)}")
+                    await session.rollback()
+                    break
 
-#             logging.info("AWB status update completed")
+            logging.info("AWB status update completed")
 
 if __name__ == "__main__":
     import uvicorn
