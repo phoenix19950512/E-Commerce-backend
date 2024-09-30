227d50694ceb8cf6bcc8c6b9411e2aa8f9a570b2
dragonwarrior61 <amodfired@gmail.com>
2024-09-29 20:04:13 -0400

    add site domain filter in refresh_data

--- refresh_data.py
+++ refresh_data.py
@@ -152,7 +152,7 @@ async def refresh_stock(db: AsyncSession = Depends(get_db)):
     async for db in get_db():
         async with db as session:
             logging.info("Starting stock refresh")
-            result = await session.execute(select(Billing_software))
+            result = await session.execute(select(Billing_software).where(Billing_software.site_domain == "samrtbill.ro"))
             db_smarts = result.scalars().all()
             if db_smarts is None:
                 logging.info("Can't find billing software")
