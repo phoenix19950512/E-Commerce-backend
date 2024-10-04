8a92c8c3c6fe87b0ce4761bd886f7636c194fdb6
dragonwarrior61 <amodfired@gmail.com>
2024-10-04 16:58:00 -0400

    change role in users

--- app/utils/role_utils.py
+++ app/utils/role_utils.py
@@ -2,7 +2,7 @@ ROLE_MAPPING = {
     -1: "UnAllowed",
     0: "Warehouse Staff",
     1: "Product Manager",
-    2: "Order Agent",
+    2: "Site Manager",
     3: "Customer Support",
     4: "Administrator",
     5: "Replacement Manager",
