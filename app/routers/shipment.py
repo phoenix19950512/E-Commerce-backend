df7955d0936565d1e728da78be2a95a0038e14f3
dragonwarrior61 <amodfired@gmail.com>
2024-10-07 15:47:58 -0400

    add HTTPException in admin supplier

--- app/routers/shipment.py
+++ app/routers/shipment.py
@@ -84,6 +84,8 @@ async def get_admin_supplier(
 ):
     result = await db.execute(select(Shipment).where(Shipment.id == shipment_id))
     db_shipement = result.scalars().first()
+    if db_shipement is None:
+        raise HTTPException(status_code=404, detail="shipment not found")
     user_id = db_shipement.user_id
     
     result = await db.execute(select(Supplier).where(Supplier.user_id == user_id))
