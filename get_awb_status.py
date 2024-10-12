2ea129b900ae97a30d49d42742d5df526c1fe2fe
dragonwarrior61 <amodfired@gmail.com>
2024-10-12 09:48:17 -0400

    add error logging when fecth returns

--- get_awb_status.py
+++ get_awb_status.py
@@ -1,143 +1,143 @@
-{
-    'isError': False, 
-    'messages': [],
-    'errors': [], 
-    'results': [
-        {
-            'is_full_fbe': 0, 
-            'emag_id': 247547, 
-            'return_parent_id': None, 
-            'order_id': '1020180582', 
-            'type': 3, 
-            'is_club': 0, 
-            'is_fast': 0, 
-            'customer_name': 'Олена Копайгоренко', 
-            'customer_company': None, 
-            'customer_phone': '0893480051', 
-            'pickup_country': 'Bulgaria', 
-            'pickup_suburb': 'Бургас', 
-            'pickup_city': 'Несебър',
-            'pickup_address': 'Хотел  Кубан, Апартаменти срещу хотел “Бор”, quarter Ж к Слънчев Бряг', 
-            'pickup_zipcode': None, 
-            'pickup_locality_id': 17344, 
-            'pickup_method': 1, 
-            'customer_account_iban': 'BG80BPBI99201001010109 ',
-            'customer_account_bank': 'Eurobank Bulgaria AD', 
-            'customer_account_beneficiary': 'Олена Копайгоренко', 
-            'replacement_product_emag_id': None, 
-            'replacement_product_id': None, 
-            'replacement_product_name': None, 
-            'replacement_product_quantity': None, 
-            'observations': None, 
-            'request_status': 3, 
-            'return_type': 3, 
-            'return_reason': 44, 
-            'date': '2024-10-10 11:45:15', 
-            'products': [
-                {
-                    'id': 407401, 
-                    'product_emag_id': 152998968,
-                    'product_id': '739062584',
-                    'quantity': 1, 
-                    'product_name': 'Органайзер за прибори, разтегателно чекмедже, регулируема опора за прибори и ножове, просторен, бял', 
-                    'diagnostic': None, 
-                    'retained_amount': None, 
-                    'return_reason': 44, 
-                    'observations': None
-                }
-            ], 
-            'extra_info': {
-                'maximum_finalization_date': None, 
-                'first_pickup_date': '2024-10-11', 
-                'estimated_product_pickup': None, 
-                'estimated_product_reception': None
-            }, 
-            'return_tax_value': '15.0000', 
-            'swap': None, 
-            'return_address_id': 44875, 
-            'country': 'Romania', 
-            'address_type': 'return', 
-            'return_address_snapshot': 'Str. Sf. Calinic Nr. 5 A, Pantelimon, Ilfov', 
-            'awbs': [{'reservation_id': 338564553}], 
-            'status_history': [
-                {'code': 'confirmation', 'event_date': '2024-10-10 11:46:22'}, 
-                {'code': 'confirmation', 'event_date': '2024-10-10 11:46:24'}, 
-                {'code': 'picked_by_courier', 'event_date': '2024-10-11 11:53:36'}
-            ], 
-            'request_history': [
-                {'date': '10 Oct 2024, 11:46', 'id': 247547, 'user': 'spasunka.kostova', 'action': 'Returul a fost creat cu status Nou', 'action_type': 'STATUS_INFO_RMA_CREATED', 'source': 'UI'}, 
-                {'date': '10 Oct 2024, 11:46', 'id': 247547, 'user': 'spasunka.kostova', 'action': 'Cererea de ridicare colet a fost generata automat.', 'action_type': 'STATUS_PICKUP_ORDER_CREATED_AUTOMATICALLY', 'source': 'UI'}, 
-                {'date': '10 Oct 2024, 11:46', 'id': 247547, 'user': 'spasunka.kostova', 'action': 'Statusul cererii a fost schimbat automat din Nou in Preluat ca urmare a emiterii unui AWB de pickup.', 'action_type': 'STATUS_INFO_STATUS_CHANGED_AUTOMATICALLY', 'source': 'UI'}
-            ], 
-            'locker': []
-        }, 
-        {
-            'is_full_fbe': 0, 
-            'emag_id': 247160, 
-            'return_parent_id': None, 
-            'order_id': '1020268873', 
-            'type': 3, 
-            'is_club': 0, 
-            'is_fast': 0, 
-            'customer_name': 'Rumy Garbova', 
-            'customer_company': None, 
-            'customer_phone': '0877873977', 
-            'pickup_country': 'Bulgaria', 
-            'pickup_suburb': 'Пловдив', 
-            'pickup_city': 'Пловдив-град', 
-            'pickup_address': 'бул. Васил Априлов 124А [EasyBox #22036]', 
-            'pickup_zipcode': None, 
-            'pickup_locality_id': 18464, 
-            'pickup_method': 4, 
-            'customer_account_iban': None, 
-            'customer_account_bank': None, 
-            'customer_account_beneficiary': None, 
-            'replacement_product_emag_id': None, 
-            'replacement_product_id': None, 
-            'replacement_product_name': None, 
-            'replacement_product_quantity': None, 
-            'observations': None, 
-            'request_status': 3, 
-            'return_type': 3, 
-            'return_reason': 127, 
-            'date': '2024-10-08 14:20:02',
-            'products': [
-                {
-                    'id': 407014, 
-                    'product_emag_id': 194480314, 
-                    'product_id': '739062990', 
-                    'quantity': 2, 
-                    'product_name': 'Универсална стойка за таблет и телефон за кола за облегалка за глава, съвместима с 4 устройства "7"-13", универсална, регулируема на 360°, триъгълна структура, идеална за деца или възрастни, черна', 
-                    'diagnostic': None, 
-                    'retained_amount': None, 
-                    'return_reason': 127, 
-                    'observations': None
-                }
-            ], 
-            'extra_info': {
-                'maximum_finalization_date': None, 
-                'first_pickup_date': '2024-10-10', 
-                'estimated_product_pickup': None, 
-                'estimated_product_reception': None
-            }, 
-            'return_tax_value': '0.0000', 
-            'swap': None, 
-            'return_address_id': 44875,
-            'country': 'Romania',
-            'address_type': 'return',
-            'return_address_snapshot': 'Str. Sf. Calinic Nr. 5 A, Pantelimon, Ilfov', 
-            'awbs': [{'reservation_id': 338167410}], 
-            'status_history': [
-                {'code': 'confirmation', 'event_date': '2024-10-08 14:21:03'}, 
-                {'code': 'confirmation', 'event_date': '2024-10-08 14:21:05'}, 
-                {'code': 'picked_by_courier', 'event_date': '2024-10-10 17:08:55'}
-            ], 
-            'request_history': [
-                {'date': '08 Oct 2024, 14:21', 'id': 247160, 'user': 'System', 'action': 'Returul a fost creat cu status Nou', 'action_type': 'STATUS_INFO_RMA_CREATED', 'source': 'rtr'}, 
-                {'date': '08 Oct 2024, 14:21', 'id': 247160, 'user': 'System', 'action': 'Cererea de ridicare colet a fost generata automat.', 'action_type': 'STATUS_PICKUP_ORDER_CREATED_AUTOMATICALLY', 'source': 'rtr'}, 
-                {'date': '08 Oct 2024, 14:21', 'id': 247160, 'user': 'System', 'action': 'Statusul cererii a fost schimbat automat din Nou in Preluat ca urmare a emiterii unui AWB de pickup.', 'action_type': 'STATUS_INFO_STATUS_CHANGED_AUTOMATICALLY', 'source': 'rtr'}
-            ], 
-            'locker': {'locker_hash': 'e7f238f6-8aa5-40f5-a1aa-5639a476f89a', 'locker_pin': 'PR5YJSS', 'locker_pin_interval_end': '2024-10-24 23:59:59'}
-        }
-    ]
-}
+# {
+#     'isError': False, 
+#     'messages': [],
+#     'errors': [], 
+#     'results': [
+#         {
+#             'is_full_fbe': 0, 
+#             'emag_id': 247547, 
+#             'return_parent_id': None, 
+#             'order_id': '1020180582', 
+#             'type': 3, 
+#             'is_club': 0, 
+#             'is_fast': 0, 
+#             'customer_name': 'Олена Копайгоренко', 
+#             'customer_company': None, 
+#             'customer_phone': '0893480051', 
+#             'pickup_country': 'Bulgaria', 
+#             'pickup_suburb': 'Бургас', 
+#             'pickup_city': 'Несебър',
+#             'pickup_address': 'Хотел  Кубан, Апартаменти срещу хотел “Бор”, quarter Ж к Слънчев Бряг', 
+#             'pickup_zipcode': None, 
+#             'pickup_locality_id': 17344, 
+#             'pickup_method': 1, 
+#             'customer_account_iban': 'BG80BPBI99201001010109 ',
+#             'customer_account_bank': 'Eurobank Bulgaria AD', 
+#             'customer_account_beneficiary': 'Олена Копайгоренко', 
+#             'replacement_product_emag_id': None, 
+#             'replacement_product_id': None, 
+#             'replacement_product_name': None, 
+#             'replacement_product_quantity': None, 
+#             'observations': None, 
+#             'request_status': 3, 
+#             'return_type': 3, 
+#             'return_reason': 44, 
+#             'date': '2024-10-10 11:45:15', 
+#             'products': [
+#                 {
+#                     'id': 407401, 
+#                     'product_emag_id': 152998968,
+#                     'product_id': '739062584',
+#                     'quantity': 1, 
+#                     'product_name': 'Органайзер за прибори, разтегателно чекмедже, регулируема опора за прибори и ножове, просторен, бял', 
+#                     'diagnostic': None, 
+#                     'retained_amount': None, 
+#                     'return_reason': 44, 
+#                     'observations': None
+#                 }
+#             ], 
+#             'extra_info': {
+#                 'maximum_finalization_date': None, 
+#                 'first_pickup_date': '2024-10-11', 
+#                 'estimated_product_pickup': None, 
+#                 'estimated_product_reception': None
+#             }, 
+#             'return_tax_value': '15.0000', 
+#             'swap': None, 
+#             'return_address_id': 44875, 
+#             'country': 'Romania', 
+#             'address_type': 'return', 
+#             'return_address_snapshot': 'Str. Sf. Calinic Nr. 5 A, Pantelimon, Ilfov', 
+#             'awbs': [{'reservation_id': 338564553}], 
+#             'status_history': [
+#                 {'code': 'confirmation', 'event_date': '2024-10-10 11:46:22'}, 
+#                 {'code': 'confirmation', 'event_date': '2024-10-10 11:46:24'}, 
+#                 {'code': 'picked_by_courier', 'event_date': '2024-10-11 11:53:36'}
+#             ], 
+#             'request_history': [
+#                 {'date': '10 Oct 2024, 11:46', 'id': 247547, 'user': 'spasunka.kostova', 'action': 'Returul a fost creat cu status Nou', 'action_type': 'STATUS_INFO_RMA_CREATED', 'source': 'UI'}, 
+#                 {'date': '10 Oct 2024, 11:46', 'id': 247547, 'user': 'spasunka.kostova', 'action': 'Cererea de ridicare colet a fost generata automat.', 'action_type': 'STATUS_PICKUP_ORDER_CREATED_AUTOMATICALLY', 'source': 'UI'}, 
+#                 {'date': '10 Oct 2024, 11:46', 'id': 247547, 'user': 'spasunka.kostova', 'action': 'Statusul cererii a fost schimbat automat din Nou in Preluat ca urmare a emiterii unui AWB de pickup.', 'action_type': 'STATUS_INFO_STATUS_CHANGED_AUTOMATICALLY', 'source': 'UI'}
+#             ], 
+#             'locker': []
+#         }, 
+#         {
+#             'is_full_fbe': 0, 
+#             'emag_id': 247160, 
+#             'return_parent_id': None, 
+#             'order_id': '1020268873', 
+#             'type': 3, 
+#             'is_club': 0, 
+#             'is_fast': 0, 
+#             'customer_name': 'Rumy Garbova', 
+#             'customer_company': None, 
+#             'customer_phone': '0877873977', 
+#             'pickup_country': 'Bulgaria', 
+#             'pickup_suburb': 'Пловдив', 
+#             'pickup_city': 'Пловдив-град', 
+#             'pickup_address': 'бул. Васил Априлов 124А [EasyBox #22036]', 
+#             'pickup_zipcode': None, 
+#             'pickup_locality_id': 18464, 
+#             'pickup_method': 4, 
+#             'customer_account_iban': None, 
+#             'customer_account_bank': None, 
+#             'customer_account_beneficiary': None, 
+#             'replacement_product_emag_id': None, 
+#             'replacement_product_id': None, 
+#             'replacement_product_name': None, 
+#             'replacement_product_quantity': None, 
+#             'observations': None, 
+#             'request_status': 3, 
+#             'return_type': 3, 
+#             'return_reason': 127, 
+#             'date': '2024-10-08 14:20:02',
+#             'products': [
+#                 {
+#                     'id': 407014, 
+#                     'product_emag_id': 194480314, 
+#                     'product_id': '739062990', 
+#                     'quantity': 2, 
+#                     'product_name': 'Универсална стойка за таблет и телефон за кола за облегалка за глава, съвместима с 4 устройства "7"-13", универсална, регулируема на 360°, триъгълна структура, идеална за деца или възрастни, черна', 
+#                     'diagnostic': None, 
+#                     'retained_amount': None, 
+#                     'return_reason': 127, 
+#                     'observations': None
+#                 }
+#             ], 
+#             'extra_info': {
+#                 'maximum_finalization_date': None, 
+#                 'first_pickup_date': '2024-10-10', 
+#                 'estimated_product_pickup': None, 
+#                 'estimated_product_reception': None
+#             }, 
+#             'return_tax_value': '0.0000', 
+#             'swap': None, 
+#             'return_address_id': 44875,
+#             'country': 'Romania',
+#             'address_type': 'return',
+#             'return_address_snapshot': 'Str. Sf. Calinic Nr. 5 A, Pantelimon, Ilfov', 
+#             'awbs': [{'reservation_id': 338167410}], 
+#             'status_history': [
+#                 {'code': 'confirmation', 'event_date': '2024-10-08 14:21:03'}, 
+#                 {'code': 'confirmation', 'event_date': '2024-10-08 14:21:05'}, 
+#                 {'code': 'picked_by_courier', 'event_date': '2024-10-10 17:08:55'}
+#             ], 
+#             'request_history': [
+#                 {'date': '08 Oct 2024, 14:21', 'id': 247160, 'user': 'System', 'action': 'Returul a fost creat cu status Nou', 'action_type': 'STATUS_INFO_RMA_CREATED', 'source': 'rtr'}, 
+#                 {'date': '08 Oct 2024, 14:21', 'id': 247160, 'user': 'System', 'action': 'Cererea de ridicare colet a fost generata automat.', 'action_type': 'STATUS_PICKUP_ORDER_CREATED_AUTOMATICALLY', 'source': 'rtr'}, 
+#                 {'date': '08 Oct 2024, 14:21', 'id': 247160, 'user': 'System', 'action': 'Statusul cererii a fost schimbat automat din Nou in Preluat ca urmare a emiterii unui AWB de pickup.', 'action_type': 'STATUS_INFO_STATUS_CHANGED_AUTOMATICALLY', 'source': 'rtr'}
+#             ], 
+#             'locker': {'locker_hash': 'e7f238f6-8aa5-40f5-a1aa-5639a476f89a', 'locker_pin': 'PR5YJSS', 'locker_pin_interval_end': '2024-10-24 23:59:59'}
+#         }
+#     ]
+# }
