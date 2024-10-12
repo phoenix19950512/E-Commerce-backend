c2dedf84eec3d82f8ac5a3a5fd8580ac56cdc263
dragonwarrior61 <amodfired@gmail.com>
2024-10-12 09:30:19 -0400

    testing emag returns

--- app/utils/emag_returns.py
+++ app/utils/emag_returns.py
@@ -158,67 +158,72 @@ async def insert_rmas_into_db(rmas, place:str, user_id, api_key):
         """).format(sql.Identifier("returns"))
 
         for rma in rmas:
-            emag_id = rma.get('emag_id') if rma.get('emag_id') else 0
-            order_id = rma.get('order_id') if rma.get('order_id') else 0
-            type = rma.get('type') if rma.get('type') else 0
-            customer_name = rma.get('customer_name') if rma.get('customer_name') else ""
-            customer_company = rma.get('customer_company') if rma.get('customer_company') else ""
-            customer_phone = rma.get('customer_phone') if rma.get('customer_phone') else ""
-            products = [str(product.get('product_id')) if product.get('product_id') else 0 for product in rma.get('products')]
-            quantity = [int(product.get('quantity')) if product.get('quantity') else 0 for product in rma.get('products')]
-            observations = [str(product.get('observations')) if product.get('observations') else "" for product in rma.get('products')]
-            pickup_address = rma.get('pickup_address') if rma.get('pickup_address') else ""
-            return_reason = str(rma.get('return_reason')) if rma.get('return_reason') else ""
-            return_type = rma.get('return_type') if rma.get('return_type') else 0
-            replacement_product_emag_id = rma.get('replacement_product_emag_id') if rma.get('replacement_product_emag_id') else 0
-            replacement_product_id = rma.get('replacement_product_id') if rma.get('replacement_product_id') else 0
-            replacement_product_name = rma.get('replacement_product_name') if rma.get('replacement_product_name') else ""
-            replacement_product_quantity = rma.get('replacement_product_quantity') if rma.get('replacement_product_quantity') else 0
-            date = rma.get('date') if rma.get('date') else ""
-            request_status = rma.get('request_status') if rma.get('request_status') else 0
-            return_market_place = place
-            awbs = rma.get('awbs')
-            if awbs and len(awbs) > 0:
-                reservation_id = awbs[0].get('reservation_id') if awbs[0] else ''
-                if reservation_id:
-                    response = get_awb(reservation_id, api_key)
-                    if response is None:
-                        awb = ""
+            try:
+                emag_id = rma.get('emag_id') if rma.get('emag_id') else 0
+                order_id = rma.get('order_id') if rma.get('order_id') else 0
+                type = rma.get('type') if rma.get('type') else 0
+                customer_name = rma.get('customer_name') if rma.get('customer_name') else ""
+                customer_company = rma.get('customer_company') if rma.get('customer_company') else ""
+                customer_phone = rma.get('customer_phone') if rma.get('customer_phone') else ""
+                products = [str(product.get('product_id')) if product.get('product_id') else 0 for product in rma.get('products')]
+                quantity = [int(product.get('quantity')) if product.get('quantity') else 0 for product in rma.get('products')]
+                observations = [str(product.get('observations')) if product.get('observations') else "" for product in rma.get('products')]
+                pickup_address = rma.get('pickup_address') if rma.get('pickup_address') else ""
+                return_reason = str(rma.get('return_reason')) if rma.get('return_reason') else ""
+                return_type = rma.get('return_type') if rma.get('return_type') else 0
+                replacement_product_emag_id = rma.get('replacement_product_emag_id') if rma.get('replacement_product_emag_id') else 0
+                replacement_product_id = rma.get('replacement_product_id') if rma.get('replacement_product_id') else 0
+                replacement_product_name = rma.get('replacement_product_name') if rma.get('replacement_product_name') else ""
+                replacement_product_quantity = rma.get('replacement_product_quantity') if rma.get('replacement_product_quantity') else 0
+                date = rma.get('date') if rma.get('date') else ""
+                request_status = rma.get('request_status') if rma.get('request_status') else 0
+                return_market_place = place
+                awbs = rma.get('awbs')
+                if awbs and len(awbs) > 0:
+                    reservation_id = awbs[0].get('reservation_id') if awbs[0] else ''
+                    if reservation_id:
+                        response = get_awb(reservation_id, api_key)
+                        if response is None:
+                            awb = ""
+                        else:
+                            awb = response.get('results').get('awb')[0].get('awb_number') if response.get('results').get('awb') and len(response.get('results').get('awb')) > 0 else ""
+                            awb_status = str(response.get('results').get('status')) if response.get('results').get('status') else ""
                     else:
-                        awb = response.get('results').get('awb')[0].get('awb_number') if response.get('results').get('awb') and len(response.get('results').get('awb')) > 0 else ""
-                        awb_status = str(response.get('results').get('status')) if response.get('results').get('status') else ""
+                        awb = ""
                 else:
                     awb = ""
-            else:
-                awb = ""
-            user_id = user_id
+                user_id = user_id
 
-            value = (
-                emag_id,
-                order_id,
-                type,
-                customer_name,
-                customer_company,
-                customer_phone,
-                products,
-                quantity,
-                observations,
-                pickup_address,
-                return_reason,
-                return_type,
-                replacement_product_emag_id,
-                replacement_product_id,
-                replacement_product_name,
-                replacement_product_quantity,
-                date,
-                request_status,
-                return_market_place,
-                awb,
-                awb_status,
-                user_id
-            )
-            cursor.execute(insert_query, value)
-            conn.commit()
+                value = (
+                    emag_id,
+                    order_id,
+                    type,
+                    customer_name,
+                    customer_company,
+                    customer_phone,
+                    products,
+                    quantity,
+                    observations,
+                    pickup_address,
+                    return_reason,
+                    return_type,
+                    replacement_product_emag_id,
+                    replacement_product_id,
+                    replacement_product_name,
+                    replacement_product_quantity,
+                    date,
+                    request_status,
+                    return_market_place,
+                    awb,
+                    awb_status,
+                    user_id
+                )
+                cursor.execute(insert_query, value)
+                conn.commit()
+                
+            except Exception as inner_error:
+                logging.error(f"Failed to insert RMA with emag_id {rma}: {inner_error}")
+                continue
         
         cursor.close()
         conn.close()
