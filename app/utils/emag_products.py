4e7598f38751f76ef72b4fcf77536f426c32816e
dragonwarrior61 <amodfired@gmail.com>
2024-10-15 16:43:52 -0400

    test

--- app/utils/emag_products.py
+++ app/utils/emag_products.py
@@ -449,7 +449,7 @@ async def post_stock_emag(marketplace: Marketplace, product_id: int, stock: int)
         "value": stock
     })
     async with httpx.AsyncClient(timeout=10) as client:
-        response = await client.patch(f"{url}/{product_id}", json=data, headers=headers)
+        response = await client.patch(f"{url}/{product_id}", data=data, headers=headers)
         if response.status_code == 200:
             return response.json()
         else:
